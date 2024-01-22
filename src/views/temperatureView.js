053f6f77b2f61b93781c4075cc1cacfdf7c891c4
Fressia Barrios <fressiabarrios2@gmail.com>
2024-01-22 20:14:07 +0100

    created temperature view

---
+++ src/views/temperatureView.js
@@ -0,0 +1,17 @@
+import { 
+CITY_ID, 
+CITY_INFO_ID, 
+TEMPERATURE_ID, 
+WEATHER_CONDITION_ID
+} from "../constants.js";
+
+export const cityWeather = () => {
+  const container = document.createElement('div');
+  container.setAttribute('id', `${CITY_INFO_ID}`);
+  container.innerHTML = `
+  <h2 id=${TEMPERATURE_ID}></h2>
+  <h3 id=${CITY_ID}></h3>
+  <p id=${WEATHER_CONDITION_ID}></p>
+  `
+  return container;
+}
