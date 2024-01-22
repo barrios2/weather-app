a67ec4bf24154cbadec70b4094551c623e49652e
Fressia Barrios <fressiabarrios2@gmail.com>
2024-01-22 20:26:55 +0100

    created constants file

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+/*
+ The constants file is used to store anything that multiple files use, 
+ that should ALWAYS be the same. It is an industry standard to make 
+ these variables fully capitalized 
+ */
+export const KEY = 'ddcd5971c6039bbcee33f5aa0f819234'; // API key
+export const API = `https://api.openweathermap.org/data/2.5/weather?units=metric`; // includes temp in celsius
+
+export const SEARCH_CONTAINER_ID = 'search-container';
+export const SEARCH_ID = 'search';
+export const MY_BTN_ID = 'my-btn';
+export const CITY_INFO_ID = 'city-info';
+export const TEMPERATURE_ID = 'temperature';
+export const CITY_ID = 'city';
+export const ADDITIONAL_INFO_ID = 'additional-info';
+export const REAL_SENSATION_ID = 'real-sensation';
+export const SENSATION_TEXT_ID = 'sensation-text';
+export const WEATHER_CONDITION_ID = 'weather-condition';
+export const HUMIDITY_ID = 'humidity';
+export const HUMIDITY_TEXT_ID = 'humidity-text';
+export const ADDITIONAL_INFO_CLASS = 'additional-info-wrap';
+export const ERROR_MESSAGE_ID = 'error-message';
+export const FA_SOLID_CLASS = 'fa-solid';
+export const FA_TRIANGLE_CLASS = 'fa-triangle-exclamation';
