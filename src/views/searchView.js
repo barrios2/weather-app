09042d75ffe0256b3042b20ca40ca80c2f5f79ac
Fressia Barrios <fressiabarrios2@gmail.com>
2024-01-22 20:47:52 +0100

    wrapped header in div to apply css

--- src/views/searchView.js
+++ src/views/searchView.js
@@ -4,9 +4,13 @@ import { API, MY_BTN_ID, SEARCH_CONTAINER_ID, SEARCH_ID } from '../constants.js'
 export const searchElement = () => {
   const container = document.createElement('div');
   container.innerHTML = `
-  <h1>Weather App</h1>
-  <input id=${SEARCH_ID} placeholder='Enter city name'>
-  <button id=${MY_BTN_ID}>Search</button>
+  <div>
+    <h1>Weather App</h1>
+  </div>
+  <div>
+    <input id=${SEARCH_ID} placeholder='Enter city name'>
+    <button id=${MY_BTN_ID}>Search</button>
+  </div>
   `
   container.setAttribute('id', `${SEARCH_CONTAINER_ID}`);
   return container;
